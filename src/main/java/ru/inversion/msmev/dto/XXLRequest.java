bdff939484a9240acc94bd6ba99f36f34e8e2416
Sulimoff <ssu95a@gmail.com>
2026-06-19 22:00:48 +0300

    refactor XXLRequest

--- src/main/java/ru/inversion/msmev/dto/XXLRequest.java
+++ src/main/java/ru/inversion/msmev/dto/XXLRequest.java
@@ -1,6 +1,7 @@
 package ru.inversion.msmev.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Builder;
@@ -15,6 +16,7 @@ import java.util.UUID;
 @Value
 @Builder
 @Jacksonized
+@JsonIgnoreProperties (ignoreUnknown = true)
 @JacksonXmlRootElement(localName = "XXLRequest")
 public class XXLRequest implements IDumpable {
 
