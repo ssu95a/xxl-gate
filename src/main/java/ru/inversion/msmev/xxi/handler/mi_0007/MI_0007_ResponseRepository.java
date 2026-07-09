2da985da863476c5a38ba522cb2460f88fcd1c50
Sulimoff <ssu95a@gmail.com>
2026-07-09 23:35:32 +0300

    MI_0007_ResponseRepository 2

--- src/main/java/ru/inversion/msmev/xxi/handler/mi_0007/MI_0007_ResponseRepository.java
+++ src/main/java/ru/inversion/msmev/xxi/handler/mi_0007/MI_0007_ResponseRepository.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.InvalidMediaTypeException;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
-import ru.inversion.datacall.CallStatement;
 import ru.inversion.datacall.IDataCall;
 import ru.inversion.datacall.SQLCallBuilder;
 import ru.inversion.dataset.ParametersByName;
@@ -16,22 +15,18 @@ import ru.inversion.msmev.mi.response.item.MiItemApplyResult;
 import ru.inversion.msmev.mi.response.item.MiItemResultRepository;
 import ru.inversion.msmev.xxi.repo.XxiRepositoryExecutor;
 import ru.inversion.tc.TaskContext;
-import ru.inversion.utils.U;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.function.Function;
 
 @Repository
 @RequiredArgsConstructor
 public class MI_0007_ResponseRepository implements MiItemResultRepository
 {
-   private static final String INF_NAMESPACE =
-           "urn://mvd/gismu/RFP_ACTUAL_BANK/1.0.1";
+   private static final String INF_NAMESPACE = "urn://mvd/gismu/RFP_ACTUAL_BANK/1.0.1";
 
-   public static final URL defXml =
-           MI_0007_ResponseRepository.class.getResource("plsql/def.xml");
+   public static final URL defXml = MI_0007_ResponseRepository.class.getResource("plsql/def.xml");
 
    private final XxiRepositoryExecutor db;
    private final ObjectMapper objectMapper;
@@ -43,18 +38,11 @@ public class MI_0007_ResponseRepository implements MiItemResultRepository
    }
 
    @Override
-   public MiItemApplyResult applyItem(
-           MiAsyncResponse response,
-           MiAsyncItemResult item,
-           int itemIndex
-   )
+   public MiItemApplyResult applyItem( MiAsyncResponse response, MiAsyncItemResult item, int itemIndex )
    {
-      MI_0007_ResponseRow row =
-              prepareRow(response, item, itemIndex);
+      MI_0007_ResponseRow row = prepareRow(response, item, itemIndex);
 
-      return db.execute(
-              "MI_0007.applyItemResponse",
-              response.itemParameters(item, itemIndex),
+      return db.execute( "MI_0007.applyItemResponse", response.itemParameters(item, itemIndex),
               tc -> {
                  MiItemApplyResult result =
                          applyItemImpl(tc, row);
@@ -83,63 +71,31 @@ public class MI_0007_ResponseRepository implements MiItemResultRepository
 
       try
       {
-         mediaType =
-                 MediaType.parseMediaType(
-                         item.payload().contentType()
-                 );
+         mediaType = MediaType.parseMediaType( item.payload().contentType() );
       }
-      catch( InvalidMediaTypeException exception )
-      {
-         throw Errors.miResponseBadFormat(
-                 "Bad MI_0007 item payload media type",
-                 exception,
-                 response.itemParameters(item, index)
-         );
+      catch( InvalidMediaTypeException exception ) {
+         throw Errors.miResponseBadFormat( "Bad MI_0007 item payload media type", exception, response.itemParameters(item, index) );
       }
 
-      if( !MediaType.APPLICATION_JSON.isCompatibleWith(mediaType) )
-      {
-         throw Errors.miResponseBadFormat(
-                 "Unsupported MI_0007 item payload media type",
-                 response.itemParameters(item, index)
-         );
-      }
+      if(!MediaType.APPLICATION_JSON.isCompatibleWith(mediaType) )
+          throw Errors.miResponseBadFormat( "Unsupported MI_0007 item payload media type", response.itemParameters(item, index) );
 
       try( InputStream stream = item.payload().openStream() )
       {
-         MI_0007_ResponseRow row =
-                 objectMapper.readValue(
-                         stream,
-                         MI_0007_ResponseRow.class
-                 );
+         MI_0007_ResponseRow row = objectMapper.readValue( stream, MI_0007_ResponseRow.class );
 
-         row.setItemUuid(item.itemExternalUuid());
+         row.setItemUuid   (item.itemExternalUuid());
          row.setMessageUuid(response.messageId());
          row.setRequestUuid(response.originalRequestId());
-         row.setItemIndex(index);
+         row.setItemIndex  (index);
 
          return row;
       }
-      catch( Exception exception )
-      {
-         throw Errors.miResponseBadFormat(
-                 "Error reading MI_0007 item JSON payload",
-                 exception,
-                 response.itemParameters(item, index)
-         );
+      catch( Exception exception ) {
+         throw Errors.miResponseBadFormat( "Error reading MI_0007 item JSON payload", exception, response.itemParameters(item, index) );
       }
    }
 
-  /*
-      :request_uuid,
-      :message_uuid,
-      :item_uuid,
-      :ires_code,
-      :tres_time,
-      :cres_info,
-      :_return,
-      :res_info
- */
 
    /** */
    private MiItemApplyResult applyItemImpl( TaskContext tc, MI_0007_ResponseRow row )
