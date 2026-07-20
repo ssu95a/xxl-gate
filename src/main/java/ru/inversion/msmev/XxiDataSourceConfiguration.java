ab7ffde29e907566964e361cc670cabc7c363417
Sulimoff <ssu95a@gmail.com>
2026-07-20 22:39:20 +0300

    fix/TaskContextFactory

--- src/main/java/ru/inversion/msmev/XxiDataSourceConfiguration.java
+++ src/main/java/ru/inversion/msmev/XxiDataSourceConfiguration.java
@@ -1,18 +1,38 @@
 package ru.inversion.msmev;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(DataSourceProperties.class)
 public class XxiDataSourceConfiguration
 {
-   /** */
-   @Bean
-   public XxiDataSource xxiCredentialDataSource( DataSource dataSource, DataSourceProperties dataSourceProperties )
+   /** Штатный Hikari-пул  */
+   @Bean(name = "xxiHikariDataSource", destroyMethod = "close")
+   @ConfigurationProperties("spring.datasource.hikari")
+   public HikariDataSource xxiHikariDataSource( DataSourceProperties properties )
    {
-      return new XxiDataSource( dataSource, dataSourceProperties.determineUrl() );
+      return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+   }
+
+   /**
+    * Основной DataSource приложения.
+    *
+    * Обычный getConnection() делегируется Hikari.
+    * Credential-вариант создаёт отдельное XXI-соединение.
+    */
+   @Bean(name = "dataSource")
+   @Primary
+   public DataSource dataSource( @Qualifier("xxiHikariDataSource") HikariDataSource delegate, DataSourceProperties properties )
+   {
+      return new XxiDataSource( delegate, properties.determineUrl() );
    }
 }
